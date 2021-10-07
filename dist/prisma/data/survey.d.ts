9f2fae9b4c6d2ad5682a942162e98d793cdaa128
Christian Angelo M Sulit <chrisgelosulit@gmail.com>
2021-10-07 18:05:11 +0800

    Fixed error on json field

--- dist/prisma/data/survey.d.ts
+++ dist/prisma/data/survey.d.ts
@@ -1,6 +1,12 @@
-declare const _default: {
+declare const _default: ({
     id: string;
     question: string;
     surveyOrder: number;
-}[];
+    response: string[];
+} | {
+    id: string;
+    question: string;
+    surveyOrder: number;
+    response: {};
+})[];
 export default _default;
--- dist/prisma/data/survey.js
+++ dist/prisma/data/survey.js
@@ -5,21 +5,25 @@ exports.default = [
         id: 'a467a093-7d39-460e-96cb-1fff56ddcafd',
         question: 'Have you experienced any of the following symptoms in the past 10 days?',
         surveyOrder: 1,
+        response: ['Yes', 'No'],
     },
     {
         id: '35d6a913-eb57-4415-a080-85c116165bee',
         question: 'Have you had face-to-face contact with a probable or confirmed COVID-19 for the past 14 days?',
         surveyOrder: 2,
+        response: ['Yes', 'No'],
     },
     {
         id: '98e08c82-4c0f-4db9-ae65-7dfceb5c8700',
         question: 'Have you provided direct care for a patient with a probable or confirmed COVID-19 case for the past 14 days?',
         surveyOrder: 3,
+        response: ['Yes', 'No'],
     },
     {
         id: 'e61000d7-9313-49d7-8023-82c982ab78e4',
         question: 'May we know your vaccination status?',
         surveyOrder: 4,
+        response: {},
     },
 ];
 //# sourceMappingURL=survey.js.map
