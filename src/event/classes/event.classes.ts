918fae8f414fd79aaf04dcb4a52cb3cd9dfb5396
csulit <chrisgelosulit@gmail.com>
2021-10-12 16:45:17 +0800

    Added objet on events

--- src/event/classes/event.classes.ts
+++ src/event/classes/event.classes.ts
@@ -10,4 +10,42 @@ export class EventClass {
     type: String,
   })
   readonly eventName: string;
+
+  @ApiProperty({
+    type: String,
+  })
+  readonly eventPhoto: string;
+
+  @ApiProperty({
+    type: String,
+    required: false,
+  })
+  readonly eventDescription: string;
+
+  @ApiProperty({
+    type: Boolean,
+    required: false,
+  })
+  readonly eventActive: boolean;
+
+  @ApiProperty({
+    type: Date,
+  })
+  readonly eventDate: Date;
+
+  @ApiProperty({
+    type: 'object',
+    properties: {
+      id: {
+        type: 'number',
+      },
+      site: {
+        type: 'string',
+      },
+      floor: {
+        type: 'string',
+      },
+    },
+  })
+  readonly eventLocation: Record<string, any>;
 }
