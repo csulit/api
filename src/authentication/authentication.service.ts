ce7553c4c54e453856760fe8d473f4a6386d18cd
csulit <chrisgelosulit@gmail.com>
2021-10-12 11:30:39 +0800

    Remove update operation in rf route

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -61,35 +61,6 @@ export class AuthenticationService {
   }
 
   async validateRefreshToken(token: string, res: Response) {
-    await this.prismaClientService.survey.update({
-      where: { id: 'e61000d7-9313-49d7-8023-82c982ab78e4' },
-      data: {
-        question: 'May we know your vaccination status?',
-        surveyOrder: 4,
-        response: {
-          status: [
-            'Fully vaccinated',
-            'First dose',
-            'Second dose',
-            'Is opt not to get vaccinated',
-            'Awaiting schedule',
-          ],
-          vaccine: [
-            'Sinovac',
-            'Astrazeneca',
-            'Moderna',
-            'Pfizer',
-            'BioNTech',
-            'Janssen',
-            'Sputnik',
-            'Novavax',
-            'Others',
-          ],
-          schedule: null,
-        },
-      },
-    });
-
     if (!token) {
       throw new NotFoundException('Refresh token not found.');
     }
