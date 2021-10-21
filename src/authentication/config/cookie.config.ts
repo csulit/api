0c507f00004933be3da51dc8bb455446a3092b3b
csulit <chrisgelosulit@gmail.com>
2021-10-21 16:24:52 +0800

    samesite set to none temporarily

--- src/authentication/config/cookie.config.ts
+++ src/authentication/config/cookie.config.ts
@@ -3,7 +3,7 @@ import { CookieOptions } from 'express';
 export const cookieConfig = (seconds: number): CookieOptions => {
   return {
     expires: new Date(Date.now() + seconds),
-    sameSite: 'strict',
+    sameSite: 'none',
     httpOnly: true,
     secure: process.env.NODE_ENV === 'development' ? false : true,
   };
