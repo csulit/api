1549ad315210d230e8ff13170e0c36f6d6183fef
csulit <chrisgelosulit@gmail.com>
2021-10-24 13:37:11 +0800

    make it strict

--- src/authentication/config/cookie.config.ts
+++ src/authentication/config/cookie.config.ts
@@ -3,7 +3,7 @@ import { CookieOptions } from 'express';
 export const cookieConfig = (seconds: number): CookieOptions => {
   return {
     expires: new Date(Date.now() + seconds),
-    sameSite: process.env.NODE_ENV === 'development' ? 'none' : 'strict',
+    sameSite: 'strict',
     httpOnly: true,
     secure: process.env.NODE_ENV === 'development' ? false : true,
   };
