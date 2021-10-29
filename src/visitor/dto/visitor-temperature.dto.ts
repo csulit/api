870bb5942fc65f7576cc9d8ccbd57a04747985e6
Christian Angelo M Sulit <chrisgelosulit@gmail.com>
2021-10-29 10:18:32 +0800

    Added types to temperature dto

--- src/visitor/dto/visitor-temperature.dto.ts
+++ src/visitor/dto/visitor-temperature.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class VisitorTemperatureDTO {
   @ApiProperty({
     type: String,
     required: true,
   })
+  @IsString()
+  @IsNotEmpty()
   readonly temperature: string;
 }
