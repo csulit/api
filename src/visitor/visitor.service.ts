18ca0c9057a4549f860130e95e911621c29968cb
jyzrgandia <73985156+jyzrgandia@users.noreply.github.com>
2021-11-19 06:55:04 +0800

    Update visitor.service.ts

--- src/visitor/visitor.service.ts
+++ src/visitor/visitor.service.ts
@@ -121,7 +121,7 @@ export class VisitorService {
       where: { id: visitorId },
       select: {
         id: true,
-        date: true,
+        createdAt: true,
         clear: true,
         workType: guest || event ? false : true,
         leaveType: guest || event ? false : true,
