ad78990a2fe199143e103ee994cd6a6d758dc99d
Christian Angelo M Sulit <chrisgelosulit@gmail.com>
2022-03-01 11:25:10 +0800

    ...

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,28 +4,26 @@ const prismaClient = new PrismaClient();
 
 export async function seed() {
   await prismaClient.$transaction(async (prisma) => {
-    const surveys = await prisma.survey.count();
+    //const surveys = await prisma.survey.count();
 
-    if (!surveys) {
-      //await prisma.survey.createMany({ data: survey });
+    //await prisma.survey.createMany({ data: survey });
 
-      const x = await prisma.userQrCode.findMany();
+    const x = await prisma.userQrCode.findMany();
 
-      x.forEach(
-        async (y) =>
-          await prisma.userQrCode.update({
-            where: { id: y.id },
-            data: {
-              qrUrl: y.qrUrl.replace(
-                'cdn.kmc.solutions',
-                'kmcstorage1.blob.core.windows.net',
-              ),
-            },
-          }),
-      );
+    x.forEach(
+      async (y) =>
+        await prisma.userQrCode.update({
+          where: { id: y.id },
+          data: {
+            qrUrl: y.qrUrl.replace(
+              'cdn.kmc.solutions',
+              'kmcstorage1.blob.core.windows.net',
+            ),
+          },
+        }),
+    );
 
-      console.log('Surveys inserted!');
-    }
+    console.log('Surveys inserted!');
   });
 
   return 'Done! ';
